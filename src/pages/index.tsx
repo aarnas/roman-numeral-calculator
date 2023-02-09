78c52f81e149db6db0a58b14c8ddc795406e7b8b
aarnas <arnas.andriuskevicius@gmail.com>
2023-02-09 19:05:31 +0200

    feat: UI handling number to roman

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
 import Head from "next/head";
 
-import styles from "./index.module.css";
+import styles from "../pages/index.module.css";
+import { numberToRoman } from "../utils";
+import { ChangeEvent, useState } from "react";
+import { romanValues } from "../constants";
 
-export default function Home() {
+export default function RomanNumeralCalculator() {
+  const [roman, setRoman] = useState("");
+
+  const onNumberChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const currentNumber = Number(e.currentTarget.value);
+    currentNumber
+      ? setRoman(numberToRoman(romanValues, currentNumber))
+      : setRoman("");
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +24,12 @@ export default function Home() {
 
       <main>
         <p>Roman Numeral Calculator</p>
+        <input
+          placeholder="No. from 1 to 1000"
+          type="number"
+          onChange={onNumberChange}
+        ></input>
+        <input placeholder="Roman Numeral" value={roman} readOnly></input>
       </main>
     </div>
   );
